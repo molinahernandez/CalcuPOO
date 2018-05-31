8cedf99f1da1f4dcb1c7de79b14ad3da7e093a6b
chihe <chihe@192.168.2.11>
2018-05-30 23:52:57 -0600

    cambio

--- CalcuPOO/src/AritmeticaMain/AritmeticFactory.java
+++ CalcuPOO/src/AritmeticaMain/AritmeticFactory.java
@@ -14,7 +14,6 @@ import Main.AbstractFactory;
 public class AritmeticFactory implements AbstractFactory {
 
     public AritmeticFactory() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     public Aritmetica getAritmetica(String type) {
